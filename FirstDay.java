ed4663dbd0afd62cbde4e184b74ac0a8a5b3cb41
Durgad <durgaranganathan@rediffmail.com>
2018-09-07 23:57:27 -0700

    Firts Project

---
+++ FirstDay.java
@@ -0,0 +1,15 @@
+package com.icicibank.loans.carloans;
+
+public class FirstDay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("hello Vishaal");
+		System.out.println("hello Raj");
+		
+		
+	}
+
+	
+	}
